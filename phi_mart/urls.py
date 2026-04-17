0a9f8543874ea714ec271fe6ac3ce3f4ac66e8cb
Sunny-2787 <cse_0182320012101265@lus.ac.bd>
2026-04-17 21:22:02 +0600

    Remove debug toolbar URLs from urlpatterns

--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -1,7 +1,5 @@
-
 from django.contrib import admin
 from django.urls import path,include
-from debug_toolbar.toolbar import debug_toolbar_urls
 from .views import api_view_a
 from django.conf.urls.static import static
 from django.conf import settings
@@ -30,6 +28,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-]+ debug_toolbar_urls()
+]
 
 urlpatterns+=static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT )
